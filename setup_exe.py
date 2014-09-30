11bbbc424d61d9851b3b63e8b6dbaf8eaa037cf6
Rafael Villar Burke <pachi@ietcc.csic.es>
2014-09-30 13:15:07 +0200

    Convertir a cx_freeze con Msys2

--- setup_exe.py
+++ setup_exe.py
@@ -19,198 +19,128 @@
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
-"""Configuración de SOL"""
+"""Configuración de SOL para generar ejecutable en win32 con cx_freeze
 
-from distutils.core import setup
-import sys, os, glob
-import py2exe
-import matplotlib
+$ python setup.py build
+
+http://stackoverflow.com/questions/21002446/bundling-gtk3-with-py2exe
+http://stackoverflow.com/questions/20067856/python3-pygobject-gtk3-and-cx-freeze-missing-dlls
+
+Para ver archivos necesarios en la distribución ver:
+http://www.tarnyko.net/repo/gtk3_build_system/tutorial/gtk3_tutorial.htm
+"""
+
+import os, sys
+from glob import glob
+from cx_Freeze import setup, Executable
 from sol import __version__
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+## Collect the list of missing dll when cx_freeze builds the app
+include_dll_path = os.path.abspath(r'C:\msys32\mingw32\bin') 
+missing_dll = ['libgtk-3-0.dll',
+               'libgdk-3-0.dll',
+               'libatk-1.0-0.dll',
+               'libcairo-gobject-2.dll',
+               #'libcroco-0.6-3.dll', #SVG
+               'libffi-6.dll',
+               'libgcrypt-20.dll',
+               'libgdk_pixbuf-2.0-0.dll',
+               'libgirepository-1.0-1.dll',
+               'libgnutls-28.dll',
+               'libjpeg-8.dll',
+               #'liblzma-5.dll', #SVG
+               'libp11-kit-0.dll',
+               'libpango-1.0-0.dll',
+               'libpangocairo-1.0-0.dll',
+               'libpangoft2-1.0-0.dll',
+               'libpangowin32-1.0-0.dll',
+               'libpyglib-2.0-python2-0.dll',
+               #'librsvg-2-2.dll', #SVG
+               #'libxml2-2.dll', #SVG
+]
 
-# DLL del VC++8 redistributable para Python
-#import sys
-#sys.path.append('C:\\WINDOWS\\WinSxS\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_5090ab56bcba71c2')
-#("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
-if os.path.exists(r'C:\WINDOWS\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_5090ab56bcba71c2'):
-    MSVCCRTGLOB = r'C:\WINDOWS\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_5090ab56bcba71c2\*.*'
-# También lo he visto aquí en un XP...
-elif os.path.exists(r'C:\WINDOWS\WinSxS\x86_Microsoft.VC90.CRT_1fc8b3b9a1e18e3b_9.0.30729.4148_x-ww_d495ac4e'):
-    MSVCCRTGLOB = r'C:\WINDOWS\WinSxS\x86_Microsoft.VC90.CRT_1fc8b3b9a1e18e3b_9.0.30729.4148_x-ww_d495ac4e\*.*'
-else:
-    raise Exception("No se ha podido encontrar el MS VC90 CRT")
+## We need to add all the libraries too (for themes, etc..)
+gtk_libs = ['etc/gtk-3.0',
+            'etc/pango',
+            #'lib/gdk-pixbuf-2.0', #SVG
+            'lib/gtk-3.0',
+            'lib/girepository-1.0',
+            'share/glib-2.0',
+            'share/gtk-3.0',
+            'share/icons/hicolor',
+            'share/icons/Adwaita/22x22',
+            'share/icons/Adwaita/index.theme',
+            'share/themes/Default/gtk-3.0']
 
-#gtk file inclusion
-import gtk
-# The runtime dir is in the same directory as the module:
-GTK_RUNTIME_DIR = os.path.join(
-    os.path.split(os.path.dirname(gtk.__file__))[0], "runtime")
-assert os.path.exists(GTK_RUNTIME_DIR), "Cannot find GTK runtime data"
-GTK_THEME_DEFAULT = os.path.join("share", "themes", "Default")
-GTK_THEME_WINDOWS = os.path.join("share", "themes", "MS-Windows")
-GTK_GTKRC_DIR = os.path.join("etc", "gtk-2.0")
-GTK_GTKRC = "gtkrc"
-GTK_WIMP_DIR = os.path.join("lib", "gtk-2.0", "2.10.0", "engines")
-GTK_WIMP_DLL = "libwimp.dll"
-#GTK_ICONS = os.path.join("share", "icons") # For tango icons
-GTK_ICONS = os.path.join("share", "icons", "hicolor") # To exclude Tango icons
-#GTK_LOCALE_DATA = os.path.join("share", "locale") # Localisation data
-GTK_LOCALE_DATA = os.path.join("share", "locale", "es") # Localisation data redux
+## Create the list of includes as cx_freeze likes
+include_files = []
+for dll in missing_dll:
+    include_files.append((os.path.join(include_dll_path, dll), dll))
 
-# you'll need to copy the etc, lib and share directories from your GTK+ install
-# from lib/ only all the *.dlls in the subtree are needed, and from
-# share/ only themes/ and locale/. saves lots of space
+## Let's add ui and data dirs from ViSol
+pats = ['ui/*.jpg', 'ui/*.png', 'res/pantallazo*.png']
+staticfiles = ['ui/sol.ui',
+               'ui/visol.cfg',
+               'data/test.res',
+               'data/test.re2',
+               'README.rst',
+               'NEWS.txt',
+               'HACKING.txt',
+               'COPYING.txt'
+]
+include_files.extend(ff for ffpat in pats for ff in glob(ffpat))
+for ff in staticfiles:
+    include_files.append(ff)
 
-# share/icons/Tango se puede eliminar (26MB) cambiando el tema por defecto a hicolor
+## Let's add gtk libraries folders and files
+for lib in gtk_libs:
+    include_files.append((os.path.join(r'C:\msys32\mingw32', lib), lib))
 
-# Receta sacada de http://stackoverflow.com/questions/7884959/bundling-gtk-resources-with-py2exe
-# para gtk3+ ver http://stackoverflow.com/questions/21002446/bundling-gtk3-with-py2exe
-def generate_data_files(prefix, tree, file_filter=None):
-    """
-    Walk the filesystem starting at "prefix" + "tree", producing a list of files
-    suitable for the data_files option to setup(). The prefix will be omitted
-    from the path given to setup(). For example, if you have
+base = None
 
-        C:\Python26\Lib\site-packages\gtk-2.0\runtime\etc\...
+## Lets not open the console while running the app
+if sys.platform == "win32":
+    base = "Win32GUI"
 
-    ...and you want your "dist\" dir to contain "etc\..." as a subdirectory,
-    invoke the function as
+executables = [
+    Executable("bin/visol",
+               base=base
+    )
+]
 
-        generate_data_files(
-            r"C:\Python26\Lib\site-packages\gtk-2.0\runtime",
-            r"etc")
+buildOptions = dict(
+    compressed=False,
+    includes=["gi"],
+    excludes=["Tkinter", "tcl", "PyQt5"],
+    packages=["gi"],
+    include_files=include_files
+    icon='res/visol.ico'
+    )
 
-    If, instead, you want it to contain "runtime\etc\..." use:
-
-        generate_data_files(
-            r"C:\Python26\Lib\site-packages\gtk-2.0",
-            r"runtime\etc")
-
-    Empty directories are omitted.
-
-    file_filter(root, fl) is an optional function called with a containing
-    directory and filename of each file. If it returns False, the file is
-    omitted from the results.
-    """
-    data_files = []
-    for root, dirs, files in os.walk(os.path.join(prefix, tree)):
-        to_dir = os.path.relpath(root, prefix)
-
-        if file_filter is not None:
-            file_iter = (fl for fl in files if file_filter(root, fl))
-        else:
-            file_iter = files
-
-        data_files.append((to_dir, [os.path.join(root, fl) for fl in file_iter]))
-
-    non_empties = [(to, fro) for (to, fro) in data_files if fro]
-
-    return non_empties
-
-data_files = (
-    # Use the function above...
-    generate_data_files(GTK_RUNTIME_DIR, GTK_THEME_DEFAULT) +
-    generate_data_files(GTK_RUNTIME_DIR, GTK_THEME_WINDOWS) +
-    generate_data_files(GTK_RUNTIME_DIR, GTK_ICONS) +
-    # ...or include single files manually
-    [
-        (GTK_GTKRC_DIR, [
-            os.path.join(GTK_RUNTIME_DIR,
-                         GTK_GTKRC_DIR,
-                         GTK_GTKRC)
-        ]),
-
-        (GTK_WIMP_DIR, [
-            os.path.join(
-                GTK_RUNTIME_DIR,
-                GTK_WIMP_DIR,
-                GTK_WIMP_DLL)
-        ]),
-        ('ui', glob.glob('ui/*.png')),
-        ('ui', glob.glob('ui/*.jpg')),
-        ('ui', ['ui/sol.ui', 'ui/visol.cfg']),
-        ('data', ['data/test.res', 'data/test.re2']),
-        ('res', glob.glob('res/pantallazo*.png')),
-        ('.', ['README.rst', 'NEWS.txt', 'HACKING.txt', 'COPYING.txt']),
-        (".", glob.glob(MSVCCRTGLOB)) # dlls del MSVCCRT90
-    ] +
-    matplotlib.get_py2exe_datafiles()
-)
-# mirar si se puede reducir el mpl-data, que tiene muchos MB
-
-windows = [{'script': 'bin/visol',
-            'icon_resources': [(1, "res/visol.ico")],
-}]
-
-options = {'py2exe': { 'packages':'encodings',
-                       'dist_dir': 'dist',
-                       # Optionally omit gio, gtk.keysyms, and/or rsvg if you're not using them
-                       #'includes': 'cairo, pango, pangocairo, atk, gobject, gio, gtk.keysyms, rsvg',
-                       'includes': 'cairo, pango, pangocairo, atk, gobject, gio' + ', matplotlib, numpy',
-                       'excludes': ['_gtkagg', '_tkagg', 'tcl', 'Tkinter',
-                                    # Eliminamos unos cuantos de la stdlib y varios instalados habitualmente
-                                    'doctest', 'pdb',
-                                    '_ssl', 'wx', 'pickle',
-                                    'pythonwin', 'pywin32', 'pythoncom',
-                                    'win32api', 'win32evtlog', 'win32', 'win32com', 'win32comext',
-                                    'win32pdh', 'win32pipe', 'win32ui', 'PIL'],
-                       "dll_excludes": ['tcl85.dll', 'tk85.dll',
-                                        'w9xpopen.exe', # solo para win95/98
-                                        'libglade-2.0-0.dll',
-                                        # Basura de mi equipo
-                                        'API-MS-Win-Core-Debug-L1-1-0.dll',
-                                        'API-MS-Win-Core-DelayLoad-L1-1-0.dll',
-                                        'API-MS-Win-Core-ErrorHandling-L1-1-0.dll',
-                                        'API-MS-Win-Core-File-L1-1-0.dll',
-                                        'API-MS-Win-Core-Handle-L1-1-0.dll',
-                                        'API-MS-Win-Core-Heap-L1-1-0.dll',
-                                        'API-MS-Win-Core-Interlocked-L1-1-0.dll',
-                                        'API-MS-Win-Core-IO-L1-1-0.dll',
-                                        'API-MS-Win-Core-LibraryLoader-L1-1-0.dll',
-                                        'API-MS-Win-Core-Localization-L1-1-0.dll',
-                                        'API-MS-Win-Core-LocalRegistry-L1-1-0.dll',
-                                        'API-MS-Win-Core-Misc-L1-1-0.dll',
-                                        'API-MS-Win-Core-ProcessEnvironment-L1-1-0.dll',
-                                        'API-MS-Win-Core-ProcessThreads-L1-1-0.dll',
-                                        'API-MS-Win-Core-Profile-L1-1-0.dll',
-                                        'API-MS-Win-Core-String-L1-1-0.dll',
-                                        'API-MS-Win-Core-Synch-L1-1-0.dll',
-                                        'API-MS-Win-Core-SysInfo-L1-1-0.dll',
-                                        'DNSAPI.dll',
-                                        #'MSIMG32.DLL',
-                                        'NSI.dll',
-                                        'USP10.DLL',
-                                        'POWRPROF.dll',
-                                        ],
-                   }
-       }
-
-setup(name='visol',
+README = open('README.rst').read()
+NEWS = open('NEWS.txt').read()
+setup(
+    name="visol",
+    author="Rafael Villar Burke",
+    author_email='pachi@rvburke.com',
     version=__version__,
     description="Visor de archivos de resultados de LIDER",
     long_description=README + '\n\n' + NEWS,
-    classifiers=[
+    options=dict(build_exe=buildOptions),
+    executables=executables,
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: MacOS X',
-    'Environment :: Win32 (MS Windows)',
-    'Environment :: X11 Applications :: GTK',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-    'Natural Language :: Spanish',
-    'Programming Language :: Python :: 2.7'
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: Scientific/Engineering'
-    ],
-    keywords='LIDER,energía,edificación,CTE',
-    author='Rafael Villar Burke',
-    author_email='pachi@rvburke.com',
-    url='http://www.rvburke.com/software.html',
-    license='GPL-2.0+',
-    windows=windows,
-    options=options,
-    data_files=data_files
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Natural Language :: Spanish',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering'],
+    keywords=u"LIDER,energía,edificación,CTE",
+    url="http://www.rvburke.com/software.html",
+    license="GPL-2.0+"
 )
