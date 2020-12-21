921a513defbc992a865238e51fc408c1688ea4cb
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-12-21 12:38:13 +0100

    Refactoriza imports

--- sol/gtkui.py
+++ sol/gtkui.py
@@ -33,9 +33,9 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf
 
 import sol
-import util
-from widgets import HistoMeses, HistoElementos, PieGlobal, ZonasGraph
-from solmodel import VISOLModel
+from sol import util
+from sol.widgets import HistoMeses, HistoElementos, PieGlobal, ZonasGraph
+from sol.solmodel import VISOLModel
 
 TESTFILE = util.get_resource('data/test.res')
 
